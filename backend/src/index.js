c41b5663134534b707aba31497e5fc76e89995d7
udaykryadav <udaykryadav092004@gmail.com>
2025-07-31 11:47:36 +0530

    project complted

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import path from "path";
+
+
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +14,7 @@ import {app,server} from "./lib/socket.js"; // Import the socket server
 dotenv.config();
 
 const PORT =process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // or higher if needed
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -24,6 +29,14 @@ app.use(cors(
  
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist","dist", "index.html"));
+    });
+}   
 
 server.listen(PORT,()=>{
     console.log("server is running on PORT:"+PORT);
--- backend/src/seeds/user.seed.js
+++ backend/src/seeds/user.seed.js
@@ -36,44 +36,9 @@ const seedUsers = [
     password: "123456",
     profilePic: "https://randomuser.me/api/portraits/women/5.jpg",
   },
-  {
-    email: "mia.johnson@example.com",
-    fullName: "Mia Johnson",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-  {
-    email: "charlotte.williams@example.com",
-    fullName: "Charlotte Williams",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/women/7.jpg",
-  },
-  {
-    email: "amelia.garcia@example.com",
-    fullName: "Amelia Garcia",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/women/8.jpg",
-  },
 
   // Male Users
-  {
-    email: "james.anderson@example.com",
-    fullName: "James Anderson",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/men/1.jpg",
-  },
-  {
-    email: "william.clark@example.com",
-    fullName: "William Clark",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/men/2.jpg",
-  },
-  {
-    email: "benjamin.taylor@example.com",
-    fullName: "Benjamin Taylor",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/men/3.jpg",
-  },
+  
   {
     email: "lucas.moore@example.com",
     fullName: "Lucas Moore",
@@ -92,12 +57,7 @@ const seedUsers = [
     password: "123456",
     profilePic: "https://randomuser.me/api/portraits/men/6.jpg",
   },
-  {
-    email: "daniel.rodriguez@example.com",
-    fullName: "Daniel Rodriguez",
-    password: "123456",
-    profilePic: "https://randomuser.me/api/portraits/men/7.jpg",
-  },
+  
 ];
 
 const seedDatabase = async () => {
