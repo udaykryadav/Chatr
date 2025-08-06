72020b9d7a917bcb5e4532b6ed390dd1af9f61cd
udaykryadav <udaykryadav092004@gmail.com>
2025-08-06 16:07:05 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +12,8 @@ import {app,server} from "./lib/socket.js"; // Import the socket server
 dotenv.config();
 
 const PORT =process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // or higher if needed
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -24,6 +28,12 @@ app.use(cors(
  
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT,()=>{
     console.log("server is running on PORT:"+PORT);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
 
 
-const BASE_URL = 'http://localhost:5001';
+const BASE_URL = import.meta.MODE==="development"?'http://localhost:5001': "/";
 
 export const useAuthStore = create((set, get) => ({
 
